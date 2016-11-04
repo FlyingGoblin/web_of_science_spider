6f19ad26d3af235a0f5c06ba98afa4d65d564860
FlyingGoblin <cylily93@gmail.com>
2016-11-04 21:34:23 +0800

    去掉无用的东西

--- Main.py
+++ Main.py
@@ -23,15 +23,3 @@ if __name__=="__main__":
             continue
         print('Cited paper count: %d'% len(cite_papers))
         paper_excel_writer.write_paper_cite(paper, cite_papers)
-
-# import xlwt as ExcelWrite
-# def writeXLS(file_name):
-#     value = [["name", "jim", "hmm", "lilei"], ["sex", "man", "woman", "man"], ["age", 19, 24, 24], ["country", "USA", "CHN", "CHN"]]
-#     xls = ExcelWrite.Workbook()
-#     sheet = xls.add_sheet("Sheet1")
-#     for i in range(0, 4):
-#         for j in range(0, len(value)):
-#             sheet.write(j, i, value[i][j])
-#     xls.save(file_name)
-# if __name__ == "__main__":
-#     writeXLS("./test_write.xls")
